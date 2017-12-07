c7b1e371fcff24152d7c3f8ecdcb5da7bfed010c
hss01248 <hss01248@gmail.com>
2017-12-07 23:36:34 +0800

    定义样式configbean

---
+++ dialog/src/main/java/com/hss01248/dialog/config/OtherStyleConfig.java
@@ -0,0 +1,16 @@
+package com.hss01248.dialog.config;
+
+import android.support.annotation.ColorRes;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class OtherStyleConfig {
+
+    public CharSequence title;
+    public CharSequence msg;
+
+    public @ColorRes int titleTxtColor = DefaultConfig.titleTxtColor;
+    public @ColorRes int msgTxtColor = DefaultConfig.msgTxtColor;
+}
---
+++ dialog/src/main/java/com/hss01248/dialog/config/ViewConfig.java
@@ -0,0 +1,8 @@
+package com.hss01248.dialog.config;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+public class ViewConfig {
+}
