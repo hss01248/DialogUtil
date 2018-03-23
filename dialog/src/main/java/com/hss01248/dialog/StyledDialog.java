728f176ff2ab68e80be1dfaf69c9de0596f97f0a
hss01248 <hss01248@gmail.com>
2018-03-23 19:23:30 +0800

    loadingdialog置空

--- dialog/src/main/java/com/hss01248/dialog/StyledDialog.java
+++ dialog/src/main/java/com/hss01248/dialog/StyledDialog.java
@@ -86,6 +86,7 @@ public class StyledDialog  {
                 @Override
                 public void run() {
                     dismiss(loadingDialog);
+                    loadingDialog = null;
                 }
             });
         }
